b62eb78690a7a0ca9dc91050621c6e27744f4658
rohini-hs <167188786+rohini-hs@users.noreply.github.com>
2024-05-06 13:58:32 +0530

    added Hello World code

---
+++ scripts/hello-world.js
@@ -0,0 +1,14 @@
+"use strict";
+function showGreetings()
+{
+    //find the messageDiv element
+    let messageDiv = document.getElementById("messageDiv");
+
+    //set its contents to Hello World
+    messageDiv.innerText = "Hello World";
+
+
+// call the showGreeting() function when the .js file loads and this line is encountered
+}
+showGreetings();
+
